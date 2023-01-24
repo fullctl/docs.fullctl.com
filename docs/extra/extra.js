d79d4a8f91b45ba04739bd9290aa29eb00de601c
Jason Carvalho <jason@20c.com>
2023-01-24 21:58:39 +0530

    add styles for image-content ol

--- docs/extra/extra.js
+++ docs/extra/extra.js
@@ -10,3 +10,42 @@ const fixSidebarHeight = new ResizeObserver(() => {
 
 sidebar.setAttribute("data-new-height", sidebar.clientHeight);
 fixSidebarHeight.observe(sidebar);
+
+let list_with_imgs = document.querySelectorAll("ol > li > img");
+const appliedOls = new Set();
+const appliedLis = new Set();
+list_with_imgs.forEach((e) => {
+  let liElement = e.parentElement;
+  let olElement = liElement.parentElement;
+
+  if (!appliedOls.has(olElement)) {
+    olElement.classList.add("text-img-ol");
+    appliedOls.add(olElement);
+  }
+
+  if (!appliedLis.has(liElement)) {
+    liElement.classList.add("text-img-li");
+    const rangeBefore = document.createRange()
+    rangeBefore.setStart(liElement, 0);
+    rangeBefore.setEndBefore(e);
+
+    const newParent = document.createElement('div');
+    newParent.classList.add('text');
+
+    rangeBefore.surroundContents(newParent);
+
+    const container = document.createElement('div');
+    container.classList.add('left-container');
+
+    rangeBefore.surroundContents(container);
+
+    rangeBefore.setStartBefore(e);
+    rangeBefore.setEndAfter(e);
+
+    const right = document.createElement('div');
+    right.classList.add('right-container');
+    rangeBefore.surroundContents(right);
+
+    appliedLis.add(liElement);
+  }
+});
