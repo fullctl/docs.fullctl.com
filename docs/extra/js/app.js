8280dc4e0b2b8e21aedae87ab16a0e596d74b383
Jason Mishike <98209208+jason20c@users.noreply.github.com>
2023-04-28 16:43:20 +0530

    Connect forms to aaactl contact backend (#36)
    * change forms backend to aaaCtl contact form
    
    * change button text
    
    * update form_url

--- docs/extra/js/app.js
+++ docs/extra/js/app.js
@@ -87,13 +87,81 @@ fullctl.application.Modal = $tc.extend(
 );
 
 
+/**
+ * Sends request to aaactl through contact backend
+ * (https://github.com/fullctl/aaactl/blob/contact-backend/docs/contact-form.md)
+ *
+ * @class fullctl.application.ContactForm
+ * @extends twentyc.rest.Form
+ */
+
+fullctl.application.ContactForm = twentyc.cls.extend(
+  "ContactForm",
+  {
+    ContactForm : function(jq, type="general", extra_info) {
+      const base_url = jq.data("api-base");
+      this.type = type;
+      this.extra_info = extra_info;
+      this.form_action = jq.data("api-action");
+      this.submit_inline = jq.data("submit-inline") == "yes";
+      this.Widget(base_url, jq);
+    },
+
+    payload : function() {
+      const data = {};
+      this.element.find('[data-api-submit="yes"]').each(function() {
+        $(this).find("input,select,textarea").each(function() {
+          const input = $(this)
+          if(input.attr("type") == "checkbox") {
+            if(input.prop("checked"))
+              data[input.attr("name")] = true;
+            else
+              data[input.attr("name")] = false;
+          } else {
+            if (input.data("type") == "int") {
+              data[input.attr("name")] = parseInt(input.val());
+            } else if(input.data("type") == "bool") {
+              data[input.attr("name")] = (input.val().toLowerCase() == "true" ?  true : false);
+            } else {
+              data[input.attr("name")] = input.val();
+            }
+          }
+        });
+      });
+
+      const message = {};
+      Object.keys(data).forEach(function(key) {
+        if (!(key == "email" || key == "name")) {
+          message[key] = data[key];
+          delete data[key]
+        }
+      });
+
+      data["message"] = message;
+      data["type"] = this.type;
+      if (this.extra_info) {
+        Object.keys(this.extra_info).forEach((key) => {
+          data.message[key] = this.extra_info[key];
+        })
+      }
+
+      return data;
+    }
+
+  },
+  twentyc.rest.Form
+)
+
+
 fullctl.application.ModalRequestDemo = $tc.extend(
   "ModalRequestDemo",
   {
     ModalRequestDemo: function (product="peerctl") {
-      var modal = this;
-      var form = this.form = new twentyc.rest.Form(
-        fullctl.template("form_request_demo")
+      const modal = this;
+      const form = this.form = new fullctl.application.ContactForm(
+        fullctl.template("form_request_demo"),
+        "general",
+        {form: "request-demo"}
       );
 
       $(this.form).on("api-write:success", (ev, e, payload, response) => {
@@ -109,7 +177,7 @@ fullctl.application.ModalRequestDemo = $tc.extend(
 );
 
 $('[data-element="request-demo-btn"]').click(function() {
-  let product = $(this).attr('data-product');
+  const product = $(this).attr('data-product');
   if(product) {
     new fullctl.application.ModalRequestDemo(product);
   } else {
@@ -121,9 +189,11 @@ fullctl.application.ModalContactUs = $tc.extend(
   "ModalContactUs",
   {
     ModalContactUs: function () {
-      var modal = this;
-      var form = this.form = new twentyc.rest.Form(
-        fullctl.template("form_contact_us")
+      const modal = this;
+      const form = this.form = new fullctl.application.ContactForm(
+        fullctl.template("form_contact_us"),
+        "general",
+        {form: "contact-us"}
       );
 
       $(this.form).on("api-write:success", (ev, e, payload, response) => {
@@ -138,11 +208,7 @@ fullctl.application.ModalContactUs = $tc.extend(
 );
 
 $('[data-element="contact-us-btn"]').click(function () {
-  if (fullctl.template("form_contact_us").attr("data-api-base")) {
-    new fullctl.application.ModalContactUs();
-  } else {
-    fullctl.application.send_mail("Contact Us");
-  }
+  new fullctl.application.ModalContactUs();
 })
 
 })(jQuery, twentyc.cls);
